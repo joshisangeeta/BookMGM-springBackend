8abeff33d242f8bb9fec353c0c19af9c5f719b82
joshisangeeta <sangeeta.joshi2@gmail.com>
2021-05-06 10:16:54 +0530

    Book MGM spring backend version on hp also later version is on HP but hp not available hence pushing

---
+++ src/main/java/com/sj/e2e/BookMgm/controller/WebController.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package com.sj.e2e.BookMgm.controller;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sj.e2e.BookMgm.dao.BookRepository;
+import com.sj.e2e.BookMgm.model.Book;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+//import antlr.collections.List;
+
+/**
+ * @author sangeeta
+ *
+ */
+@RestController
+@CrossOrigin //(origins = "http://localhost:4200/dashboard")
+//@CrossOrigin(origins = "*")
+@RequestMapping("/web")
+public class WebController {
+
+	    @Autowired BookRepository bookRepo;
+	    @GetMapping("/books")
+	   // @CrossOrigin(origins = "http://localhost:4200")
+	    @CrossOrigin(origins = "*")
+	    
+	    public List<Book> home()
+	    {
+	    	System.out.println("in home..");
+	    //	ArrayList<Book> books  = new ArrayList<Book>();
+	    	
+	    	//   books = (ArrayList<Book>) bookRepo.findAll();
+	    	 // System.out.println("books+"+ books);
+	      //  model.addAttribute("books", bookRepo.findAll());
+	      //  return books.toArray();
+	        return bookRepo.findAll();
+	       // return "index";
+	    }
+	    
+	    @GetMapping("/latestbooks")
+	    public List<Book> dashboard()
+	    {
+	    	System.out.println("in home..");
+	    //	ArrayList<Book> books  = new ArrayList<Book>();
+	    	
+	    	//   books = (ArrayList<Book>) bookRepo.findAll();
+	    	 // System.out.println("books+"+ books);
+	      //  model.addAttribute("books", bookRepo.findAll());
+	      //  return books.toArray();
+	        return bookRepo.findAll().subList(1, 3);
+	       // return "index";
+	    }
+	    
+	    @PutMapping("/books")
+	    public Book addBook(@RequestBody Book book) {
+	    	
+	    	 return bookRepo.save(book);
+	    	
+	    }
+	    
+	    @GetMapping("/delbooks/{id}")
+	    public List<Book> deleteBook(@PathVariable int id) {
+	    	
+	    	bookRepo.delete(id);
+	    	return bookRepo.findAll();
+	    	
+	    }
+	    
+	    
+	    @GetMapping("/books/{id}")
+		   // @CrossOrigin(origins = "http://localhost:4200")
+		    @CrossOrigin(origins = "*")
+	    public Optional <Book> getById(@PathVariable Integer id){
+	    	System.out.println(id);
+	    	Optional<Book> b = bookRepo.findById(id);
+	    	System.out.println(b);
+	    	return b;
+	    }
+	    
+	    
+	    
+	    
+	    
+	
+}
